f8774082c0168241bf72cf0dd848450e3e7a7cf3
Chris Muthig <camuthig@gmail.com>
2018-09-22 15:05:44 -0700

    Create a basic user model with validation

---
+++ migrations/20180922005100_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    email VARCHAR UNIQUE NOT NULL,
+    name VARCHAR NOT NULL,
+    nickname VARCHAR NOT NULL,
+    password_hash BYTEA,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ models/user.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/uuid"
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// A User gives our application a way to track accounts in the application.
+type User struct {
+	ID           uuid.UUID `json:"id" db:"id"`
+	Email        string    `json:"email" db:"email"`
+	Name         string    `json:"name" db:"name"`
+	Nickname     string    `json:"nickname" db:"nickname"`
+	Password     string    `json:"-" db:"-"`
+	PasswordHash []byte    `json:"-" db:"password_hash"`
+}
+
+func (u *User) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	v := []validate.Validator{
+		&validators.EmailIsPresent{Field: u.Email, Name: "Email"},
+		&validators.StringIsPresent{Field: u.Name, Name: "Name"},
+		&validators.StringIsPresent{Field: u.Nickname, Name: "Nickname"},
+	}
+
+	if u.Password != "" {
+		v = append(v, &validators.StringLengthInRange{Field: u.Password, Name: "Password", Min: 8})
+	}
+
+	return validate.Validate(
+		v...,
+	), nil
+}
+
+func (u *User) BeforeSave(tx *pop.Connection) error {
+	if u.Password != "" {
+		hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+
+		if err != nil {
+			return err
+		}
+
+		u.PasswordHash = hash
+	}
+
+	return nil
+}
